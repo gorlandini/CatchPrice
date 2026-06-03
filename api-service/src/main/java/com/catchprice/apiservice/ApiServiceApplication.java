b67721eeaddf5eff1ce941105bf3c7856acc01d0
Guilherme Orlandini <guilherme.orlandini@gmail.com>
2026-06-02 22:01:33 -0300

    chore: devtools + page serialization + application yml fixes

--- api-service/src/main/java/com/catchprice/apiservice/ApiServiceApplication.java
+++ api-service/src/main/java/com/catchprice/apiservice/ApiServiceApplication.java
@@ -2,8 +2,13 @@ package com.catchprice.apiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+
+@EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ApiServiceApplication {
 
     public static void main(String[] args) {
